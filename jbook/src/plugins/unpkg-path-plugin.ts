1c9ee75e48890dab19683b62bf3c67c9fd050009
Kabooley <35451559+Kabooley@users.noreply.github.com>
2022-06-04 21:55:53 +0900

    plugin able to fetch network module

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,32 +1,57 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
-  return {
-    name: 'unpkg-path-plugin',
-    setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
-      });
+    return {
+        name: 'unpkg-path-plugin',
+        setup(build: esbuild.PluginBuild) {
+            // path解決を示すパート
+            build.onResolve({ filter: /.*/ }, async (args: any) => {
+                console.log('onResolve', args);
+                if (args.path === 'index.js') {
+                    return { path: args.path, namespace: 'a' };
+                }
+                // NOTE: unpkg.comでtiny-test-pkgというとりあえずのモジュールを取得してみる
+                //
+                // pathの解決方法はonResolveに
+                else if (args.path === 'tiny-test-pkg') {
+                    return {
+                        path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+                        namespace: 'a',
+                    };
+                }
+            });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+            // pathからファイルを返すパート
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
+                console.log('onLoad', args);
 
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: `
-              import message from './message';
-              console.log(message);
-            `,
-          };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
-        }
-      });
-    },
-  };
+                if (args.path === 'index.js') {
+                    return {
+                        loader: 'jsx',
+                        // tiny-test-pkgからモジュールを取得するので以下は無効化
+            //             contents: `
+            //   import message from 'tiny-test-pkg';
+            //   console.log(message);
+            // `,
+                      // 
+                        contents: `
+                          const message = require('tiny-test-pkg');
+                          console.log(message);
+                          `,
+                    };
+                }
+
+                // axiosでpathからモジュールを取得する
+                const { data } = await axios.get(args.path);
+                console.log(data);
+
+                // 取得したデータを返す
+                return {
+                    loader: 'jsx',
+                    contents: data,
+                };
+            });
+        },
+    };
 };

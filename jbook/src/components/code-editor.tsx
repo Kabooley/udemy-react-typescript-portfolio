280917b28436ae0d56cc3acca210c931c04a7f03
Kabooley <35451559+Kabooley@users.noreply.github.com>
2022-06-28 04:27:00 +0900

    Fixed differences between v4 monaco-editor/react

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,35 +1,42 @@
-import MonacoEditor from '@monaco-editor/react';
+import { useRef } from "react";
+import MonacoEditor from "@monaco-editor/react";
 
 interface CodeEditorProps {
-    initialValue: string;
-    onChange(value: string): void;
+  initialValue: string;
+  onChange(value: string): void;
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
-    const onEditorDidMount = (getValue: () => string, monaco: Monaco) => {
-        console.log(getValue());
-    };
+  const editorRef = useRef(null);
+  const onEditorDidMount = (editor: any, monacoEditor: any) => {
+    editorRef.current = editor;
+    editor.onDidChangeModelContent(() => {
+      if (!editorRef.current) return;
+      console.log(editor.getValue());
+      onChange(editor.getValue());
+    });
+  };
 
-    return (
-        <MonacoEditor
-            // editorDidMount={onEditorDidMount}
-            onMount={onEditorDidMount}
-            value={initialValue}
-            height="500px"
-            theme="vs-dark"
-            language="javascript"
-            options={{
-                wordWrap: 'on',
-                minimap: { enabled: false },
-                showUnused: false,
-                folding: false,
-                lineNumbersMinChars: 3,
-                fontSize: 16,
-                scrollBeyondLastLine: false,
-                automaticLayout: true,
-            }}
-        />
-    );
+  return (
+    <MonacoEditor
+      // editorDidMount={onEditorDidMount}
+      onMount={onEditorDidMount}
+      value={initialValue}
+      height="500px"
+      theme="vs-dark"
+      language="javascript"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,52 +1,49 @@
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
-import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
-import CodeEditor from './components/code-editor';
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 const App = () => {
-    const ref = useRef<any>();
-    const iframe = useRef<any>();
-    const [input, setInput] = useState('');
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState("");
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-        });
-    };
-    useEffect(() => {
-        startService();
-    }, []);
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
 
-        iframe.current.srcdoc = html;
+    iframe.current.srcdoc = html;
 
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window',
-            },
-        });
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
 
-        // setCode(result.outputFiles[0].text);
-        iframe.current.contentWindow.postMessage(
-            result.outputFiles[0].text,
-            '*'
-        );
-    };
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+  };
 
-    const html = `
+  const html = `
     <html>
       <head></head>
       <body>
@@ -66,19 +63,22 @@ const App = () => {
     </html>
   `;
 
-    return (
-        <div>
-            <CodeEditor initialValue="const a = 1;" />
-            <textarea
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            ></textarea>
-            <div>
-                <button onClick={onClick}>Submit</button>
-            </div>
-            <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
-        </div>
-    );
+  return (
+    <div>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+    </div>
+  );
 };
 
 // ReactDOM.render(<App />, document.querySelector('#root'));
